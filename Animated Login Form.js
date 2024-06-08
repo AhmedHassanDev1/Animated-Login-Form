bf34c0c695c7c100c81dfb8d12973411616f6746
AhmedHassanDev1 <133208592+Ahmedhassano@users.noreply.github.com>
2024-06-08 18:20:14 +0300

    -v 1.0.0

---
+++ Animated Login Form.js
@@ -0,0 +1,6 @@
+let box_container=document.querySelector(".box_container")
+let element
+for (let i = 0; i < 300; i++) {
+    element=document.createElement("div")
+    box_container.appendChild(element)
+}
